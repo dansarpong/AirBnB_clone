563ddcf48f118fa185ad343e1f29f6f87ef8c7d6
dannycypher <dansarpong.me@gmail.com>
2024-02-10 05:38:39 +0000

    Add FileStorage class for serializing and deserializing instances

---
+++ models/engine/file_storage.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" Defines FileStorage which serializes and deserializes instances """
+import json
+from models.base_model import BaseModel
+
+classes = {
+    "BaseModel": BaseModel
+    }
+
+
+class FileStorage:
+    """ Class that serializes and deserializes
+    to-and-fro instances and a JSON file """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ Returns the __objects dictionary """
+
+        return self.__objects
+
+    def new(self, obj):
+        """ Sets obj in __objects """
+
+        if obj is not None:
+            key = type(self).__name__ + "." + obj.id
+            self.__objects[key] = obj
+
+    def save(self):
+        """ Serializes __objects to __file_path """
+
+        obj = {}
+        for key in self.__objects:
+            obj[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(obj, f)
+
+    def reload(self):
+        """ Deserializes __file_path to __objects """
+
+        try:
+            with open(self.__file_path, "r") as f:
+                obj_dict = json.load(f)
+            for key, value in obj_dict.items():
+                self.__objects[key] = classes[value["__class__"]](**value)
+        except Exception:
+            pass
